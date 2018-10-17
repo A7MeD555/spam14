2fae191e6deb5b83af36e5426270f1605ab97063
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-10-17 14:17:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,8 +10,12 @@ client.on('ready', () => {
 
 
 
-
-// CODES HERE
+client.on("ready", () => {
+let channel =     client.channels.get("501507487954894860")
+setInterval(function() {
+channel.send(`اديلو اديلو متسبهوش اديلو سبام سبام سبام احية احية احية احية :joy:`);
+}, 25)
+})
 
 
 
